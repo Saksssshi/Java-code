6c688e9f0c76d4705add9199663334d72b6d350b
Saksssshi <113081570+Saksssshi@users.noreply.github.com>
2022-09-22 15:14:42 +0530

    Add files via upload

---
+++ Banking.java
@@ -0,0 +1,24 @@
+package bankOverriding;
+
+public class Banking {
+	
+	void clientId()
+	{
+		int a = 10091;
+		int b = 10092;
+		int c = 10093;
+		System.out.println("First Client Id is : " + a);
+		System.out.println("Second Client Id is : " + b);
+		System.out.println("Third Client Id is : " + c);
+	}
+	void clientName()
+	{
+		String a = "Sakshi Gupta";
+		String b = "Raj";S
+		String c = "Ashish";
+		System.out.println("First Client name is : " + a);
+		System.out.println("Second Client name is : " + b);
+		System.out.println("Third Client name is : " + c);
+	}
+
+}
