dda013a1a574cc9fabb5458d9ba32ced81d658a3
Saksssshi <113081570+Saksssshi@users.noreply.github.com>
2022-10-06 14:29:55 +0530

    Add files via upload

---
+++ Practical/Child.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+public class Child extends Parent
+{
+void Speak()	
+{	
+System.out.println("I Love Speaking");
+}
+
+void Cook()	
+{
+System.out.println("I Love Cooking");
+}
+}
---
+++ Practical/Parent.java
@@ -0,0 +1,12 @@
+package Constructor;
+
+public class Parent {
+Parent()
+{
+System.out.println("Its Default Constructor");	
+}
+void Sing()
+{
+System.out.println("I Love Singing");	
+}
+}
